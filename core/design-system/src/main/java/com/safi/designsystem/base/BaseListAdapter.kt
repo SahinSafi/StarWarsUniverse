ea077fc49950fe469c3977206899e7b76e1bee8c
Safi <ensahinsafi@gmai.com>
2023-11-18 21:05:19 +0600

    feat(ListAdapter): generic list adapter implement.

---
+++ core/design-system/src/main/java/com/safi/designsystem/base/BaseListAdapter.kt
@@ -0,0 +1,37 @@
+package com.safi.designsystem.base
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+/**
+ * A generic RecyclerView ListAdapter that uses Data Binding & DiffUtil.
+ *
+ * @param <T> Type of the items in the list
+ * @param <V> The type of the ViewDataBinding
+ */
+ abstract class BaseListAdapter<T,V : ViewBinding>(
+    diffCallback: DiffUtil.ItemCallback<T>
+ ) : ListAdapter<T, BaseViewHolder<V>>(diffCallback){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<V> {
+        val binding = createBinding(parent)
+        return BaseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<V>, position: Int) {
+
+        bind(holder.binding, getItem(position), position)
+    }
+
+    protected abstract fun createBinding(parent: ViewGroup): V
+    protected abstract fun bind(binding: V, item: T, position: Int)
+}
+
+/**
+ * A generic ViewHolder that works with a [ViewBinding].
+ * @param <T> The type of the ViewDataBinding.
+ */
+class BaseViewHolder<out T : ViewBinding> constructor(val binding: T) : RecyclerView.ViewHolder(binding.root)
